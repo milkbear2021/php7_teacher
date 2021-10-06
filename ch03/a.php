4749028ee405168681bc5572010d7b037f7f1dd7
milkbear2019 <milkbear2019@gmail.com>
2021-10-06 11:37:19 +0800

    表單傳資料

--- ch03/a.php
+++ ch03/a.php
@@ -15,21 +15,29 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-4 offset-4" style="background-color:lightgray;">
-                <form action="b.php" method="get" class="form">
-                    <div class="form-group form-inline">
-                        <label for="usename">帳號</label>
-                        <input type="text" id="username" name="username" placeholder="請輸入帳號" class="form-control">
-                    </div>
-                    <p></p>
-                    <div class="form-group form-inline">
-                        <label for="password">密碼</label>
-                        <input type="text" id="password" name="password" placeholder="請輸入密碼" class="form-control">
-                    </div>
-                    <p></p>
-                    <div class="form-group">
-                        <input type="submit" name="sub1" id="sub1" value="確定" class="btn btn-warning">
-                    </div>
+            <div class="col-4 offset-4">
+                <form action="b.php" method="post" class="form">
+                    <table class="table table-striped table-hover">
+                        <div class="form-group">
+                            <tr>
+                            <td class="col-2"><label for="username">帳號</label></td>
+                            <td><input calss="col-7" type="text" id="username" name="username" placeholder="請輸入帳號" class="form-control"></td>
+                            </tr>
+                        </div>
+                        <p></p>
+                        <div class="form-group">
+                            <td class="col-2"><label for="password">密碼</label></td>
+                            <td><input calss="col-7" type="password" id="password" name="password" placeholder="請輸入密碼" class="form-control"></td>
+                            </tr>
+                        </div>
+                        <p></p>
+                        <div class="form-group">
+                            <tr>
+                            <td class="col-2">&nbsp;</td>
+                            <td class="col-7 "><input type="submit" name="sub1" id="sub1" value="確定" class="btn btn-sm btn-warning"></td>
+                            </tr>
+                        </div>
+                    </table>
                 </form>
             </div>
         </div>
