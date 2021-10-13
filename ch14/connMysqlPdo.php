733986b6646b8428d072c669127158d6a84dd408
milkbear2019 <milkbear2019@gmail.com>
2021-10-13 13:48:05 +0800

    PDO

--- ch14/connMysqlPdo.php
+++ ch14/connMysqlPdo.php
@@ -1,17 +1,18 @@
 <?php
 	header("Content-Type: text/html; charset=utf-8");
 	//資料庫主機設定
-	$db_host = "localhost";
+	$db_host = "localhost:3308";
 	$db_username = "root";
-	$db_password = "1234";
-	$db_name = "class";
+	$db_password = "";
+	$db_name = "class";//資料庫名稱
 	//錯誤處理
 	//$db_link = @new mysqli($db_host, $db_username, $db_password, $db_name);
 	try{
 		//連線資料庫
 		$db_link =new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", $db_username, $db_password);
 	} catch (PDOException $e) {
-	    print "資料庫連結失敗，訊息: {$e->getMessage()}<br/>";
+	    //print "資料庫連結失敗，訊息: {$e->getMessage()}<br/>";
+		echo "資料庫連結失敗，訊息: {$e->getMessage()}<br/>";
     	die();
     }
 ?>
--- ch14/php_mysql20.php
+++ ch14/php_mysql20.php
@@ -1,9 +1,9 @@
 ﻿<?php 
 	header("Content-Type: text/html; charset=utf-8");
 	include("connMysqlPdo.php");
-	
 	$sql_query = "SELECT * FROM students";
 	$result = $db_link->query($sql_query);
+	//$result = $db_link->query("SELECT * FROM students");由3及4列合併而成
 	while($row_result=$result->fetch()){
 		foreach($row_result as $item=>$value){
 			echo $item."=".$value."<br>";
