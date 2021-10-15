71987b84ab9330f31a1a27dd57a219c49efb19cd
milkbear2019 <milkbear2019@gmail.com>
2021-10-15 10:39:38 +0800

    輪播

--- OwlCarousel_Demo/OwlCarousel_Demo_Source.php
+++ OwlCarousel_Demo/OwlCarousel_Demo_Source.php
@@ -4,42 +4,34 @@
 <head>
     <meta charset="utf-8">
     <title>OWL</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
-        .wrapper {
-            width: 100%;
-            height: auto;
-            overflow: hidden;
-        }
+    .wrapper {
+        width: 100%;
+        height: auto;
+        overflow: hidden;
+    }
 
-        .item {
-             margin: 10px;
-        }
-
+    .item {
+        margin: 10px;
+    }
     </style>
 </head>
 
 <body>
-    <div class="wrapper">
+    <div class="container-fluid">
         <div class="owl-carousel owl-theme">
-            <div class="item"><img src="img/owl1.jpg"></div>
-            <div class="item"><img src="img/owl2.jpg"></div>
-            <div class="item"><img src="img/owl3.jpg"></div>
-            <div class="item"><img src="img/owl4.jpg"></div>
-            <div class="item"><img src="img/owl5.jpg"></div>
-            <div class="item"><img src="img/owl6.jpg"></div>
-            <div class="item"><img src="img/owl7.jpg"></div>
-            <div class="item"><img src="img/owl8.jpg"></div>
-            <div class="item"><img src="img/owl11.jpg"></div>
-            <div class="item"><img src="img/owl12.jpg"></div>
-            <div class="item"><img src="img/owl13.jpg"></div>
-            <div class="item"><img src="img/owl14.jpg"></div>
-            <div class="item"><img src="img/owl15.jpg"></div>
-            <div class="item"><img src="img/owl16.jpg"></div>
-            <div class="item"><img src="img/owl17.jpg"></div>
-            <div class="item"><img src="img/owl18.jpg"></div>
+            <div class="item"><img src="img/fullimage1.jpg" class="item img-fluid"></div>
+            <div class="item"><img src="img/fullimage2.jpg" class="item img-fluid"></div>
+            <div class="item"><img src="img/fullimage3.jpg" class="item img-fluid"></div>
+            <div class="item"><img src="img/fullimage4.jpg" class="item img-fluid"></div>
+            <div class="item"><img src="img/fullimage5.jpg" class="item img-fluid"></div>
+            <div class="item"><img src="img/fullimage6.jpg" class="item img-fluid"></div>
+            <div class="item"><img src="img/fullimage7.jpg" class="item img-fluid"></div>
+
         </div>
     </div>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -52,29 +44,28 @@
     <script src="assets/owlcarousel/owl.carousel.js"></script>
 
     <script>
-        $('.owl-carousel').owlCarousel({
-            responsive: true,
-            autoplayHoverPause:true, // 滑鼠經過時暫停
-            loop: true,
-            margin: 10,
-            autoplay: true,
-            autoplayTimeout: 1000,
-            nav: true, //導航
-            navText: ["上一頁", "下一頁"], // 導航文字
+    $('.owl-carousel').owlCarousel({
+        responsive: true,
+        autoplayHoverPause: true, // 滑鼠經過時暫停
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        nav: true, //導航
+        navText: ["上一張", "下一張"], // 導航文字
 
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 3
-                },
-                1000: {
-                    items: 5
-                }
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
             }
-        })
-
+        }
+    })
     </script>
 
 </body>
