4f3e3b5139a6d55d4cb24f8b8a9251cbe8160b00
milkbear2019 <milkbear2019@gmail.com>
2021-10-18 16:46:01 +0800

    修改在index.php上的資料顯示控制

--- phpboard/adminfix.php
+++ phpboard/adminfix.php
@@ -12,88 +12,119 @@ if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
 }
 //執行更新動作
 if(isset($_POST["action"])&&($_POST["action"]=="update")){	
-	$query_update = "UPDATE board SET boardname=?, boardsex=?, boardsubject=?, boardmail=?, boardweb=?, boardcontent=? WHERE boardid=?";
+	$query_update = "UPDATE board SET boardname=?, boardsex=?, boardsubject=?, boardmail=?, boardweb=?, boardcontent=?,boardshow =? WHERE boardid=?";
 	$stmt = $db_link->prepare($query_update);
-	$stmt->bind_param("ssssssi",$boardname,$boardsex,$boardsubject,$boardmail,$boardweb,$boardcontent,$boardid);
+	$stmt->bind_param("sssssssi",$boardname,$boardsex,$boardsubject,$boardmail,$boardweb,$boardcontent,$boardshow,$boardid);
   $boardname = $_POST["boardname"];
   $boardsex = $_POST["boardsex"];
   $boardsubject = $_POST["boardsubject"];
   $boardmail = $_POST["boardmail"];
   $boardweb = $_POST["boardweb"];
   $boardcontent = $_POST["boardcontent"];
+  $boardshow = $_POST["boardshow"];
 	$boardid = $_POST["boardid"];
 	$stmt->execute();
 	$stmt->close();
 	//重新導向回到主畫面
 	header("Location: admin.php");
 }
-$query_RecBoard = "SELECT boardid, boardname, boardsex, boardsubject, boardmail, boardweb, boardcontent FROM board WHERE boardid=?";
+$query_RecBoard = "SELECT boardid, boardname, boardsex, boardsubject, boardmail, boardweb, boardcontent,boardshow FROM board WHERE boardid=?";
 $stmt=$db_link->prepare($query_RecBoard);
 $stmt->bind_param("i", $_GET["id"]);
 $stmt->execute();
-$stmt->bind_result($boardid, $boardname, $boardsex, $boardsubject, $boardmail, $boardweb, $boardcontent);
+$stmt->bind_result($boardid, $boardname, $boardsex, $boardsubject, $boardmail, $boardweb, $boardcontent,$boardshow);
 $stmt->fetch();
 ?>
 <html>
+
 <head>
-<title>訪客留言版管理系統</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="style.css" rel="stylesheet" type="text/css">
+    <title>訪客留言版管理系統</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
 </head>
+
 <body bgcolor="#ffffff">
-<table width="700" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td><table align="left" border="0" cellpadding="0" cellspacing="0" width="700">
+    <table width="700" border="0" align="center" cellpadding="0" cellspacing="0">
         <tr>
-          <td background="images/admin_topbg.jpg"><img name="admin_r1_c1" src="images/admin_r1_c1.jpg" width="465" height="36" border="0" alt=""></td>
-          <td width="15"><img name="admin_r1_c8" src="images/admin_r1_c8.jpg" width="15" height="36" border="0" alt=""></td>
+            <td>
+                <table align="left" border="0" cellpadding="0" cellspacing="0" width="700">
+                    <tr>
+                        <td background="images/admin_topbg.jpg"><img name="admin_r1_c1" src="images/admin_r1_c1.jpg"
+                                width="465" height="36" border="0" alt=""></td>
+                        <td width="15"><img name="admin_r1_c8" src="images/admin_r1_c8.jpg" width="15" height="36"
+                                border="0" alt=""></td>
+                    </tr>
+                </table>
+            </td>
         </tr>
-    </table></td>
-  </tr>
-  <tr>
-    <td><img name="admin_r2_c1" src="images/admin_r2_c1.jpg" width="700" height="28" border="0" alt=""></td>
-  </tr>
-  <tr>
-    <td background="images/admin_r3_c1.jpg"><div id="mainRegion">
-        <form name="form1" method="post" action="">
-          <table width="90%" border="0" align="center" cellpadding="4" cellspacing="0">
-            <tr valign="top">
-              <td colspan="2" class="heading">更新訪客留言版資料</td>
-            </tr>
-            <tr valign="top">
-              <td>
-    			<p>標題<input name="boardsubject" type="text" id="boardsubject" value="<?php echo $boardsubject;?>"></p>
-                <p>姓名<input name="boardname" type="text" id="boardname" value="<?php echo $boardname;?>"></p>
-                <p>性別<input name="boardsex" type="radio" id="radio" value="男" <?php if($boardsex=="男"){echo "checked";}?>>男
-                  <input name="boardsex" type="radio" id="radio2" value="女" <?php if($boardsex=="女"){echo "checked";}?>>女</p>
-                <p>郵件<input name="boardmail" type="text" id="boardmail" value="<?php echo $boardmail;?>"></p>
-                <p>網站<input name="boardweb" type="text" id="boardweb" value="<?php echo $boardweb;?>"></p>
-              </td>
-              <td align="right">
-              	<p><textarea name="boardcontent" id="boardcontent" cols="50" rows="8"><?php echo $boardcontent;?></textarea></p>
-                <p>
-                  <input name="boardid" type="hidden" id="boardid" value="<?php echo $boardid;?>">
-                  <input name="action" type="hidden" id="action" value="update">
-                  <input type="submit" name="button" id="button" value="更新資料">
-                  <input type="button" name="button3" id="button3" value="回上一頁" onClick="window.history.back();">
-                </p></td>
-            </tr>
-          </table>
-        </form>
-      </div></td>
-  </tr>
-  <tr>
-    <td><table align="left" border="0" cellpadding="0" cellspacing="0" width="700">
         <tr>
-          <td width="15"><img name="admin_r4_c1" src="images/admin_r4_c1.jpg" width="15" height="31" border="0" alt=""></td>
-          <td background="images/admin_botbg.jpg"><a href="?logout=true"><img name="admin_r4_c2" src="images/logout.jpg" width="77" height="31" border="0" alt="登出管理"></a></td>
-          <td align="right" valign="top" background="images/admin_botbg.jpg" class="trademark">© 2016 eHappy Studio All Rights Reserved. </td>
-          <td width="15"><img name="admin_r4_c8" src="images/admin_r4_c8.jpg" width="15" height="31" border="0" alt=""></td>
+            <td><img name="admin_r2_c1" src="images/admin_r2_c1.jpg" width="700" height="28" border="0" alt=""></td>
         </tr>
-      </table></td>
-  </tr>
-</table>
+        <tr>
+            <td background="images/admin_r3_c1.jpg">
+                <div id="mainRegion">
+                    <form name="form1" method="post" action="">
+                        <table width="90%" border="0" align="center" cellpadding="4" cellspacing="0">
+                            <tr valign="top">
+                                <td colspan="2" class="heading">更新訪客留言版資料</td>
+                            </tr>
+                            <tr valign="top">
+                                <td>
+                                    <p>標題<input name="boardsubject" type="text" id="boardsubject"
+                                            value="<?php echo $boardsubject;?>"></p>
+                                    <p>姓名<input name="boardname" type="text" id="boardname"
+                                            value="<?php echo $boardname;?>"></p>
+                                    <p>性別<input name="boardsex" type="radio" id="radio" value="男"
+                                            <?php if($boardsex=="男"){echo "checked";}?>>男
+                                        <input name="boardsex" type="radio" id="radio2" value="女"
+                                            <?php if($boardsex=="女"){echo "checked";}?>>女
+                                    </p>
+                                    <p>郵件<input name="boardmail" type="text" id="boardmail"
+                                            value="<?php echo $boardmail;?>"></p>
+                                    <p>網站<input name="boardweb" type="text" id="boardweb"
+                                            value="<?php echo $boardweb;?>"></p>
+                                    <p>是否顯示<input name="boardshow" type="radio" id="radio" value="Y"
+                                    <?php if($boardshow=="Y"){echo "checked";}?>>是
+                                    <input name="boardshow" type="radio" id="radio2" value="N"
+                                    <?php if($boardshow=="N"){echo "checked";}?>>否
+                                    </p>        
+                                </td>
+                                <td align="right">
+                                    <p><textarea name="boardcontent" id="boardcontent" cols="50"
+                                            rows="8"><?php echo $boardcontent;?></textarea></p>
+                                    <p>
+                                        <input name="boardid" type="hidden" id="boardid" value="<?php echo $boardid;?>">
+                                        <input name="action" type="hidden" id="action" value="update">
+                                        <input type="submit" name="button" id="button" value="更新資料">
+                                        <input type="button" name="button3" id="button3" value="回上一頁"
+                                            onClick="window.history.back();">
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <table align="left" border="0" cellpadding="0" cellspacing="0" width="700">
+                    <tr>
+                        <td width="15"><img name="admin_r4_c1" src="images/admin_r4_c1.jpg" width="15" height="31"
+                                border="0" alt=""></td>
+                        <td background="images/admin_botbg.jpg"><a href="?logout=true"><img name="admin_r4_c2"
+                                    src="images/logout.jpg" width="77" height="31" border="0" alt="登出管理"></a></td>
+                        <td align="right" valign="top" background="images/admin_botbg.jpg" class="trademark">© 2016
+                            eHappy Studio All Rights Reserved. </td>
+                        <td width="15"><img name="admin_r4_c8" src="images/admin_r4_c8.jpg" width="15" height="31"
+                                border="0" alt=""></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 </body>
+
 </html>
 <?php
 	$stmt->close();
